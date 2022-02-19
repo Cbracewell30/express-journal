a4a69045307866f220e00a9a6e589e103fafb21d
Chris <chrisbracewell35@gmail.com>
2022-02-19 14:39:20 -0500

    adding post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,33 @@
 const router = require("express").Router();
 const notes = require("../db/db.json");
-const fs = require (fs)
+const fs = require ("fs");
+const { request } = require("http");
 
 router.get("/api/notes", (req, res) => {
-    notes = 
+    //read the data file
+    notes = JSON.parse(fs.readFileSync("./db/db.json")) || []
+    
+    console.log("getroute", notes)
     res.json(notes);
 });
 
+router.post("/api/notes", (req, res) => {
+    //creating new note object
+    var newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id : Math.floor(Math.random()* 100)
+    }
+    // pushing the new info in the data
+    notes.push(newNote)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function (err){
+        if(err){
+           throw err 
+        }
+    })
+    
+    console.log("postroute", notes)
+    res.JSON(notes);
 
+});
   module.exports = router;
