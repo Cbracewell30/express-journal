1501cec1527852b58e7898cf041b633bab3d7af5
Chris <chrisbracewell35@gmail.com>
2022-02-19 14:18:21 -0500

    adding server

--- routes/noteRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { notes } = require("../develop/animals");
-
-router.get("/db", (req, res) => {
-    res.json(notes);
-});
-
-
-  module.exports = router;
